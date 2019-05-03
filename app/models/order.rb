848ab68fefa507613fabd861958bae329a21457a
nelcifran <nelcifranpires@gmail.com>
2019-05-03 11:13:18 -0300

    first commit easy

---
+++ app/models/order.rb
@@ -0,0 +1,27 @@
+class Order < ApplicationRecord
+  belongs_to :restaurant
+  has_many :order_products
+  before_validation :set_price
+  accepts_nested_attributes_for :order_products, allow_destroy: true
+
+  validates :name, presence: true
+  validates :phone_number, presence: true
+  validates :total_value, presence: true
+
+  enum status: { waiting: 0, delivered: 1 }
+
+     
+  private
+     
+  def set_price
+    @final_price = 0
+    order_products.each do |order_product|
+      product = Product.find order_product.product_id
+      @final_price += order_product.quantity * product.price
+    end
+     
+    self.total_value = @final_price
+  end
+
+
+end
